01f5c602b913576d0ac8254d5b263514e2e91c42
Agdrox <jakobleeb@gmx.at>
2020-12-09 14:52:40 +0100

    commit mit ganz vielen packages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CurrencyCalculator from './pages/CurrencyCalculator/CurrencyCalculator';
+import Clock from './pages/Clock/Clock';
+import MagicTable from './pages/MagicTable/MagicTable';
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path={['/Table', '/MagicTable']} component={MagicTable}/>
+      <Route path='/CurrencyCalculator' exact component={CurrencyCalculator}/>
+      <Route path='/' component={Clock}/>
+    </Router>
+    
   );
 }
 
